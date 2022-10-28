5ff96e2d4bbf56d025a150e64da2798c89da96c2
Edward NoaLand <edwardnoaland@gmail.com>
2022-10-28 15:55:01 +0800

    feat: finish quiz3

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -14,18 +14,105 @@
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+// --------------------------------------------------
+// solution 1
+//
+// use std::borrow::Borrow;
+// use std::fmt;
+// use std::fmt::{Formatter, write};
+//
+// pub enum GradeFormat {
+//     Numerical(f32),
+//     Alphabetical(String),
+// }
+//
+// impl fmt::Display for GradeFormat {
+//     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+//         match self {
+//             GradeFormat::Numerical(grade) => write!(f, "{}", grade),
+//             GradeFormat::Alphabetical(grade) => write!(f, "{}", grade),
+//         }
+//     }
+// }
+//
+// impl From<f32> for GradeFormat {
+//     fn from(numerical_grade: f32) -> Self {
+//         Self::Numerical(numerical_grade)
+//     }
+// }
+//
+// impl From<String> for GradeFormat {
+//     fn from(alphabetical_grade: String) -> Self {
+//         Self::Alphabetical(alphabetical_grade)
+//     }
+// }
+//
+// mod sealed {
+//     pub trait Sealed {}
+//     impl Sealed for f32 {}
+//     impl Sealed for String {}
+// }
+//
+// pub struct ReportCard<T: Into<GradeFormat> + sealed::Sealed + Clone> {
+//     pub grade: T,
+//     pub student_name: String,
+//     pub student_age: u8,
+// }
+//
+// impl<T: Into<GradeFormat> + sealed::Sealed + Clone> ReportCard<T> {
+//     pub fn print(&self) -> String {
+//         format!("{} ({}) - achieved a grade of {}",
+//             &self.student_name, &self.student_age, &self.grade.clone().into())
+//     }
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn generate_numeric_report_card() {
+//         let report_card = ReportCard {
+//             grade: 2.1,
+//             student_name: "Tom Wriggle".to_string(),
+//             student_age: 12,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Tom Wriggle (12) - achieved a grade of 2.1"
+//         );
+//     }
+//
+//     #[test]
+//     fn generate_alphabetic_report_card() {
+//         // TODO: Make sure to change the grade here after you finish the exercise.
+//         let report_card = ReportCard {
+//             grade: "A+".to_string(),
+//             student_name: "Gary Plotter".to_string(),
+//             student_age: 11,
+//         };
+//         assert_eq!(
+//             report_card.print(),
+//             "Gary Plotter (11) - achieved a grade of A+"
+//         );
+//     }
+// }
+// --------------------------------------------------
 
-pub struct ReportCard {
-    pub grade: f32,
+use std::borrow::Borrow;
+use std::fmt;
+use std::fmt::{Display, Formatter, write};
+
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+                &self.student_name, &self.student_age, &self.grade)
     }
 }
 
@@ -50,7 +137,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
