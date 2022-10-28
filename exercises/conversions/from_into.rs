3b997afc8b958eb199eb4d7b9896dcd9db65919b
Edward NoaLand <edwardnoaland@gmail.com>
2022-10-28 16:10:05 +0800

    feat: finish from_into

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,25 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let fields: Vec<&str> = s.split(',').collect();
+
+        if fields[0] == "" || fields.len() != 2 {
+            return Person::default();
+        } else {
+            let age = fields[1].parse::<usize>();
+            match age {
+                Ok(a) => Person{
+                    name: fields[0].to_string(),
+                    age: a,
+                },
+                _ => Person::default()
+            }
+        }
     }
 }
 
