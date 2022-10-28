b711d5b8e83f6a8fe15147fe5ec55ccebed66236
Edward NoaLand <edwardnoaland@gmail.com>
2022-10-28 16:01:22 +0800

    feat: finish box1

--- exercises/standard_library_types/box1.rs
+++ exercises/standard_library_types/box1.rs
@@ -16,11 +16,9 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -33,11 +31,16 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    List::Nil
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let last_element = Box::new(List::Nil);
+    let fifth = Box::new(List::Cons(5, last_element));
+    let forth = Box::new(List::Cons(4, fifth));
+    let third = Box::new(List::Cons(3, forth));
+    let second = Box::new(List::Cons(2, third));
+    List::Cons(1, second)
 }
 
 #[cfg(test)]
